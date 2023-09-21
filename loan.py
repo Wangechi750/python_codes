8c29c2d4c50803c2b660b050bdd00579629a8966
Wangechi Mwaniki <144658085+Wangechi750@users.noreply.github.com>
2023-09-21 16:50:26 +0300

    Add files via upload

---
+++ loan.py
@@ -0,0 +1,7 @@
+#loan program
+income=float(input("enter income:"))
+age=int(input("enter age:"))
+if income>=21000 and age>=18:
+    print("Congrats u qualify")
+else:
+    print("sorry, u dont qualify")
